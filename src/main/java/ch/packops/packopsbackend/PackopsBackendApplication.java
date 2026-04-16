c6cacfa109d6c193a32c6e821b67cc8aecc491b9
Kapischan Sriganthan <kapischan.sriganthan@outlook.com>
2026-04-16 14:22:21 +0200

    Initial commit

---
+++ src/main/java/ch/packops/packopsbackend/PackopsBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.packops.packopsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackopsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackopsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/packops/packopsbackend/PackopsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.packops.packopsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackopsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
